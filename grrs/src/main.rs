b7e28105ff18d708cdac7120f76e6d5cd112e1a1
Noah <burghardtnoah@gmail.com>
2023-08-12 18:52:48 -0600

    chapter 1.4

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -9,6 +9,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    println!("pattern: {}", args.pattern);
-    println!("path: {}", args.path.into_os_string().into_string().unwrap());
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file!");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line)
+        }
+    }
 }
